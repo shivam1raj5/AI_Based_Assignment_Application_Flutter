c96ce51ae35af8998dba14adfe6b4e4eb59626cc
Shivam Raj <shivam1raj5@gmail.com>
2023-11-06 18:53:04 +0530

    First UI Page Finish

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,162 @@ class _MyHomePageState extends State<MyHomePage> {
         body: CustomScrollView(slivers: [
           SliverFillRemaining(
             hasScrollBody: false,
-            child: 
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 12,
+                ),
+                const Text(
+                  "Write Essays with Powered AI",
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Color.fromARGB(255, 0, 128, 255),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 17,
+                ),
+                SizedBox(
+                  width: 370,
+                  height: 180,
+                  child: TextField(
+                    controller: _questionController,
+                    onChanged: (value) {
+                      setState(() {
+                        question = value;
+                      });
+                    },
+                    maxLines: null,
+                    expands: true,
+                    keyboardType: TextInputType.multiline,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      hintText: 'Write a best match title/prompt',
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 17,
+                ),
+                SizedBox(
+                  width: 370,
+                  height: 80,
+                  child: TextField(
+                    controller: _tagsController,
+                    onChanged: (value) {
+                      setState(() {
+                        tags = value;
+                      });
+                    },
+                    maxLines: null,
+                    expands: true,
+                    keyboardType: TextInputType.multiline,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      hintText: 'Add Tags, Keywords (Use, to add)',
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      height: 125,
+                    ),
+                    const Text(
+                      '  Choose Language',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Color.fromARGB(255, 0, 128, 255),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 40,
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 50,
+                      child: DropdownButton<String>(
+                        value: language,
+                        onChanged: (newValue) {
+                          setState(() {
+                            language = newValue!;
+                          });
+                        },
+                        items: dropdownItems.map((item) {
+                          return DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(item),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      '  Word Count',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Color.fromARGB(255, 0, 128, 255),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 110,
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 60,
+                      child: TextField(
+                        controller: _wordsizeController,
+                        onChanged: (value) {
+                          setState(() {
+                            wordsize = value;
+                          });
+                        },
+                        maxLines: null,
+                        expands: true,
+                        keyboardType: TextInputType.multiline,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          hintText: 'Eg-75',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: SizedBox(
+                          width: 380,
+                          height: 65,
+                          child: OutlinedButton(
+                              onPressed: () {
+                                setState(() {
+                                  finalQuestion =
+                                      "$question$tags$language$wordsize";
+                                });
+                              },
+                              style: OutlinedButton.styleFrom(
+                                backgroundColor: const Color.fromARGB(
+                                    255, 210, 187, 255), // Background color
+                              ),
+                              child: const Text(
+                                'Submit',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Color.fromARGB(255, 0, 128, 255)),
+                              )),
+                        ))),
+              ],
+            ),
           ),
         ]));
   }
